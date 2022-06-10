b6dde72a675162b99444add2d069e564e31e46cc
huangjie <1364045672@qq.com>
2022-06-10 14:42:43 +0800

    优化

--- src/views/Menus.vue
+++ src/views/Menus.vue
@@ -3,40 +3,50 @@
     <el-select v-model="value" clearable placeholder="请选择">
       <el-option
         v-for="item in options"
-        :key="item.value"
-        :label="item.name"
-        :value="item.value">
+        :key="item._id"
+        :label="item.name['zh-CN']"
+        :value="item._id"
+      >
       </el-option>
-  </el-select>
+    </el-select>
+    <el-table
+      :data="tableData"
+      border
+      height="200"
+      :summary-method="getSummaries"
+      show-summary
+      style="width: 100%; margin-top: 20px"
+    >
+      <el-table-column prop="id" label="ID" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名"> </el-table-column>
+      <el-table-column prop="amount1" label="数值 1（元）"> </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 //导入餐馆get请求函数，进行获取数据
-import {restaurantGet} from '@/api/restaurant/index'
-import _ from 'lodash'
+import { restaurantGet } from "@/api/restaurant/index";
 export default {
-    name:'Menus',
-    data() {
-      return {
-        options:[],
-        value: ''
-      }
-    },
-    created(){
-      //发送请求获取数据
-      restaurantGet().then(res=>{
-        //定义一个空的数组
-        let obj=[]
-        //遍历数组中每一个对象
-        res.data.forEach(item => {
-          if(item.name)
-        })
-      }).catch(err=>{
-        console.log(err)
+  name: "Menus",
+  data() {
+    return {
+      options: [],
+      value: "",
+    };
+  },
+  created() {
+    //发送请求获取数据
+    restaurantGet()
+      .then((res) => {
+        //使用get函数过滤出来
+        this.options = res.data;
       })
-    }
-}
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
 </script>
 
 <style scoped>
